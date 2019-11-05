4d1444c5f81966919251042b6925d09df259e474
Colby Allen <colbyallen012@gmail.com>
2019-11-05 15:20:05 -0700

    Updates

---
+++ precache-manifest.0de64b8346527fc16222407fd8347516.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "463053b1cbacee6c1d5b",
+    "url": "/Board-Game-App/static/css/main.db00e49c.chunk.css"
+  },
+  {
+    "revision": "463053b1cbacee6c1d5b",
+    "url": "/Board-Game-App/static/js/main.e71fba91.chunk.js"
+  },
+  {
+    "revision": "4cb9631a282560ff22b6",
+    "url": "/Board-Game-App/static/js/runtime~main.c0e35a54.js"
+  },
+  {
+    "revision": "8fde8073cbf014b24906",
+    "url": "/Board-Game-App/static/js/2.9863944f.chunk.js"
+  },
+  {
+    "revision": "d79dcf2f34060d29c0ed4373b97b4c78",
+    "url": "/Board-Game-App/static/media/One-Player.d79dcf2f.svg"
+  },
+  {
+    "revision": "0deae2d7142b9ac3a485d229074910be",
+    "url": "/Board-Game-App/static/media/Two-Players.0deae2d7.svg"
+  },
+  {
+    "revision": "5730e8cce72d8f26348260bd49be3020",
+    "url": "/Board-Game-App/static/media/Three-Players.5730e8cc.svg"
+  },
+  {
+    "revision": "027f3c366cbacf7d9eb770f35d070eac",
+    "url": "/Board-Game-App/static/media/Four-Players.027f3c36.svg"
+  },
+  {
+    "revision": "364f3264f761d368b74134c91eafb1cb",
+    "url": "/Board-Game-App/static/media/Five-Players.364f3264.svg"
+  },
+  {
+    "revision": "845c643a3b833ff4004a5129c8afa54a",
+    "url": "/Board-Game-App/static/media/Group-Players.845c643a.svg"
+  },
+  {
+    "revision": "64d252560740d5a68bab9afcd6b25117",
+    "url": "/Board-Game-App/static/media/Search.64d25256.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/Board-Game-App/static/media/Like.e87899ae.svg"
+  },
+  {
+    "revision": "ec6223055097995a57e4bc7153d946dd",
+    "url": "/Board-Game-App/static/media/Unlike.ec622305.svg"
+  },
+  {
+    "revision": "ee1d745f55f860f39ccaf14c3df1970d",
+    "url": "/Board-Game-App/index.html"
+  }
+];
